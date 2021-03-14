c2f0d7b132093e348cffbe8c94441370e992b17d
Йорданка Згурова <yz44325235@edu.mon.bg>
2021-03-14 13:44:46 +0200

    Add project files.

---
+++ Data/CarsManagerDb.cs
@@ -0,0 +1,17 @@
+﻿using Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class CarsManagerDb : DbContext
+    {
+        public virtual DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-FK4S744E\\SQLEXPRESS;Database=CarsManagerDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
